3f991e7d75dadc8594132114dcd023de21eebd4c
Vaibhav Shrivastava <shrivastava.vaibhav@thinksys.com>
2022-04-29 15:03:54 -0700

    Added Loader and added search filter

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,9 +1,12 @@
-import React from "react";
+import {React , useState} from "react";
 import { readData } from "../action";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
-import { Empreducer } from "../reducer/Empreducer";
+
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./index.css";
+
 const Data = () => {
   const dispatch = useDispatch();
   const { list, loading } = useSelector((state) => state.employee);
@@ -11,32 +14,44 @@ const Data = () => {
   useEffect(() => {
     dispatch(readData());
   }, []);
-  
-console.log(loading)
-  return (
-    <div>
-      { loading ? <div>loading</div>:
-          
-          
-          <table className="table table-dark">
-        <thead>
-          <tr>
-            <th scope="col">MAKE_ID</th>
-            <th scope="col">MAKE_NAME</th>
-          </tr>
-        </thead>
 
-        
-          {list.map((ele) => (
-            <tbody>
-              <tr>
+  const [search, setsearch] = useState("")
+  const searchList=list;
+  console.log(search.toLowerCase())
+  return (
+    
+    <div>
+      <div> 
+        <input type ="text" placeholder="search"
+        onChange={(e)=>{setsearch(e.target.value)}} />
+        </div>
+      {loading ? (
+        <div className="loader"></div>
+      ) : (
+        <table className="table table-dark">
+          <thead>
+            <tr>
+              <th scope="col">MAKE_ID</th>
+              <th scope="col">MAKE_NAME</th>
+            </tr>
+          </thead>
+
+          {searchList.filter((ele)=>{
+            if (search==""){
+              return ele
+            }else if(ele.Make_Name.includes(search.toLowerCase())){
+              return ele
+            }
+          }).map((ele) => (
+            <tbody key={ele.key}>
+              <tr >
                 <td>{ele.Make_ID}</td>
                 <td>{ele.Make_Name}</td>
               </tr>
             </tbody>
-          ))
-        }
-      </table>}
+          ))}
+        </table>
+      )}
     </div>
   );
 };
